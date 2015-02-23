d841b9d6f643b1ed89006ee53cf185ecf03b6936
StephanieBeaton <stefanybeaton@gmail.com>
2015-02-23 11:30:18 -0800

    node server.js works, added models/User.js, tweaked userRoutes, added .gitignore

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,27 +1,29 @@
-var model = require('../models/_____');
-var eatAuth = require('eatAuth');
-var bodyparser = require('body-parser');
+'use strict';
 
-module.exports = function(app, appSecret){
+var bodyparser = require('body-parser');
+var User = require('../models/User');
+
+
+module.exports = function(app){
 	app.use(bodyparser.json());
-	
+
 	app.get('', function(req, res){
 		model.find({}, function(err, data){
 			if(err) return res.status(500).send({msg: 'could not retrieve data'});
-			
+
 			res.join(data);
 		});
 	});
 	app.post('', function(req, res){
-		
+
 	});
 	app.put('', function(req, res){
-		
+
 	});
 	app.patch('', function(req, res){
-		
+
 	});
 	app.delete('', function(req, res){
-		
+
 	});
 };
