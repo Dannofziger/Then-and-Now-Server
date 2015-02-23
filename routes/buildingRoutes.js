6619355620d932585d3a0d76e422d9466c82318b
StephanieBeaton <stefanybeaton@gmail.com>
2015-02-23 11:56:57 -0800

    added models/Building.js and routes/buildingRoutes.js shell and updated server.js for buildingRoutes.js

---
+++ routes/buildingRoutes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var bodyparser = require('body-parser');
+var Building = require('../models/Building');
+
+
+module.exports = function(app){
+  app.use(bodyparser.json());
+
+  app.get('', function(req, res){
+    model.find({}, function(err, data){
+      if(err) return res.status(500).send({msg: 'could not retrieve data'});
+
+      res.join(data);
+    });
+  });
+  app.post('', function(req, res){
+
+  });
+  app.put('', function(req, res){
+
+  });
+  app.patch('', function(req, res){
+
+  });
+  app.delete('', function(req, res){
+
+  });
+};
