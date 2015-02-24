ba5aa05a9dce2350c4df0dc298642d92ed8fa5f8
StephanieBeaton <stefanybeaton@gmail.com>
2015-02-24 11:14:37 -0800

    changed building get to fetch all buildings within a radius

--- routes/buildingRoutes.js
+++ routes/buildingRoutes.js
@@ -2,32 +2,88 @@
 
 var bodyparser = require('body-parser');
 var Building = require('../models/Building');
-
+var url = require('url');
+var queryString = require('querystring');
+var distance = require('../lib/calc_dist_btwn_two_pts_long_lat');
 
 module.exports = function(app){
   app.use(bodyparser.json());
 
   // request to get all the buildings from the database
   app.get('/building', function(req, res) {
-    // .find() is mongoose static method on the "class" Building
+    console.log("in buildingRoutes.js  app.get()");
+    console.dir(req);
 
-    console.log("in buildingRoutes.js about to call Building.find()");
-    // We haven't added  "user_id" field to  models/Building.js  yet
-    // Building.find({user_id: req.user._id}, function(err, data) {
-    Building.find({}, function(err, data) {
-      // fetch failed
-      if (err) {
-        console.log("in buildingRoutes.js  app.get() Building.find err = " + err);
-        return res.status(500).send({'msg': 'could not retrieve buildings'});
-      }
-      console.log("success in Building.find()");
-      console.log("data");
-      console.log(data);
-      console.dir(data);
+    // get the values of the parameters from the Request url
+    //localhost:3000/api/v1/building?gettype=invicinity&radius=.025&long=-122.348905&lat=47.6205535 get
 
-      // success - return array of all the platypuses to the client
-      res.json(data);
-    });
+    console.log("req.url");
+    console.log(req.url);
+    var params = url.parse(req.url).query;
+    console.log("url.parse(req.url).query");
+    console.log(params);
+
+    var getType = queryString.parse(params)["gettype"];
+    console.log("getType = " + getType);
+
+    if (getType === "invicinity") {
+      var vicinityRadius = queryString.parse(params)["radius"]; // in miles
+      console.log("vicinityRadius = " + vicinityRadius);
+      var reqLong = queryString.parse(params)["long"];
+      var reqLat = queryString.parse(params)["lat"];
+
+     // reqLat = 47.6205535 get      remove "get"
+      var substrReqLat = reqLat.substring(0, reqLat.length - 4);
+      console.log("reqLat substring = " + substrReqLat);
+      console.log("param reqLong = " + reqLong);
+      console.log("param reqLat = "  + reqLat);
+
+      console.log("in buildingRoutes.js about to call Building.find()");
+      // .find() is mongoose static method on the "class" Building
+      // We haven't added  "user_id" field to  models/Building.js  yet
+      // Building.find({user_id: req.user._id}, function(err, data) {
+      Building.find({}, function(err, data) {
+        // fetch failed
+        if (err) {
+          console.log("in buildingRoutes.js  app.get() Building.find err = " + err);
+          return res.status(500).send({'msg': 'could not retrieve buildings'});
+        }
+        console.log("success in Building.find()");
+        console.log("data");
+        //console.log(data);
+        //console.dir(data);
+
+        var dist = 0;
+        var inVicinity = [];
+
+        // loop thru all the array of buildings and
+        // ... create another array of buildings
+        // ... within the rectangle around lat. and long.
+        // ... sent in the request
+        for (var i = 0; i < data.length; i++) {
+          console.log("data[i].longitude");
+          console.log(data[i].longitude);
+          console.log("data[i].latitude");
+          console.log(data[i].latitude);
+          console.log("param reqLong = " + reqLong);
+          console.log("param reqLat = "  + reqLat);
+          dist = distance(data[i].longitude, data[i].latitude,
+            reqLong,reqLat,'M');
+
+          // if building is "in the vicinity" of Lat and Long
+          // ... in request, then add to inVicinity array
+          if (dist && dist < vicinityRadius) {
+            inVicinity.push(data[i]);
+          }
+
+        }  //  for (var i = 0; i < data.length; i++) {
+
+        // success - return array of all the platypuses to the client
+        //res.json(data);
+        res.json(inVicinity);
+      });
+
+    }
   });
 
   // request to insert new buiilding in the database
