f6e2d57f1cd2e63871995d2bc9d3eef5141857a9
Dannofziger <dannofziger@gmail.com>
2015-02-26 11:49:51 -0800

    Changed API to handle JSON objects, rather than arguements in a url string

--- routes/buildingRoutes.js
+++ routes/buildingRoutes.js
@@ -1,13 +1,70 @@
 'use strict';
 
-var bodyparser = require('body-parser');
 var Building = require('../models/Building');
-var url = require('url');
-var queryString = require('querystring');
-var distance = require('../lib/calc_dist_btwn_two_pts_long_lat');
+//var url = require('url');
+//var queryString = require('querystring');
+//var distance = require('../lib/calc_dist_btwn_two_pts_long_lat');
+var bodyparser = require('body-parser');
 
 module.exports = function(app){
   app.use(bodyparser.json());
+
+  app.get('/building', function(req, res){
+    console.log('req: '+ JSON.stringify(req.body));
+    var type = req.body.gettype;
+    if(type === 'tags'){
+      var tagArr = req.body.tagsArray;
+      var tagArray = [];
+      for(var i = 0; i < tagArr.length; i++){
+        tagArray[i] = tagArr[i].tag.toLowerCase();
+      };
+      console.log(tagArray);
+      Building.find({})
+        .where({'tags':{
+          $in: tagArray
+        }})
+        .exec(function(err, data){
+          if(err) return res.status(500).send({msg: 'could not get data'});
+          console.log(data);
+          res.json(data);
+        });
+    }else if(type === 'rectangleSearch'){
+      var ULLon = req.body.upperLeftLong;
+      var ULLat = req.body.upperLeftLat;
+      var LRLon = req.body.lowerRightLong;
+      var LRLat = req.body.lowerRightLat;
+      Building.find({ loc:
+       { $geoWithin :
+         { $geometry:
+           { type : "Polygon",
+             coordinates : [ [ 
+              [ULLon, ULLat],
+              [ULLon, LRLat],
+              [LRLon, LRLat],
+              [LRLon, ULLat],
+              [ULLon, ULLat]  ] ]
+            }
+          }
+        }
+      }, function(err, data){
+      if(err) return res.status(500).send({msg: 'could not get data'});
+      res.json(data);
+     })
+    }
+  });
+//  app.put();
+  app.post('/building', function(req, res){
+    var newBuilding = new Building(req.body).
+    newBuilding.save(function(err, data){
+      if(err) return res.status(500).send({msg: 'could not write data'});
+      res.json(data);
+    });
+  });
+//  app.patch();
+//  app.delete();
+};
+/*
+module.exports = function(app){
 
   // request to get all the buildings from the database
   app.get('/building', function(req, res) {
@@ -250,3 +307,4 @@ module.exports = function(app){
   });
 
 };
+*/
--- server.js
+++ server.js
@@ -3,37 +3,36 @@
 var express = require('express');
 var mongoose = require('mongoose');
 //var passport = require('passport');
-var userRoutes = require('./routes/userRoutes');
+//var userRoutes = require('./routes/userRoutes');
 var buildingRoutes = require('./routes/buildingRoutes');
-var passport = require('passport');
 
-console.log("process.env.MONGO_URI = " + process.env.MONGO_URI);
-console.log('mongodb://localhost/tan_development')
+//console.log("process.env.MONGO_URI = " + process.env.MONGO_URI);
+//console.log('mongodb://localhost/tan_development')
 var connectResult = mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/tan_development');
-console.log('connectResult = ' + connectResult);
+//console.log('connectResult = ' + connectResult);
 
 var app = express();
-app.use(passport.initialize());
-require('./lib/passport_strat')(passport);
+//app.use(passport.initialize());
+//require('./lib/passport_strat')(passport);
 
-var userRouter = express.Router();
+//var userRouter = express.Router();
 var buildingRouter = express.Router();
 
-console.log("in server.js calling userRoutes(userRouter)");
+//console.log("in server.js calling userRoutes(userRouter)");
 //userRoutes(userRouter, passport, app.get('appSecret'));
-userRoutes(userRouter, passport);
+//userRoutes(userRouter), passport);
 
-console.log("in server.js calling buildingRoutes(buildingRouter)");
+//console.log("in server.js calling buildingRoutes(buildingRouter)");
 buildingRoutes(buildingRouter);
 
 
-console.log("in server.js calling app.use(userRouter)");
-app.use('/api/v1', userRouter);
+//console.log("in server.js calling app.use(userRouter)");
+//app.use('/api/v1', userRouter);
 
-console.log("in server.js calling app.use(buildingRouter)");
+//console.log("in server.js calling app.use(buildingRouter)");
 app.use('/api/v1', buildingRouter);
 
-console.log("in server.js starting webserver by calling app.listen()");
+//console.log("in server.js starting webserver by calling app.listen()");
 // start up the app to listen for incoming requests on PORT environment variable OR  3000
 app.listen(process.env.PORT || 3000, function() {
   console.log('server listening on port ' + (process.env.PORT || 3000));
